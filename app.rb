d269ee8b58c600ad925caee07bca3fa9310605ba
momota <makoto.momota@gmail.com>
2014-05-11 09:47:01 +0900

    replace by find_or_create_by

--- app.rb
+++ app.rb
@@ -13,13 +13,8 @@ ActiveRecord::Base.establish_connection(db_conf["db"]["development"])
 test_name = "momota.txt"
 test_url  = "http://momota.github.io/"
 
-rec = Hoges.find_by url: test_url
-unless rec
-  hoge = Hoges.new { |h|
-    h.name = test_name
-    h.url  = test_url
-  }
-  hoge.save
+rec = Hoges.find_or_create_by( url: test_url ) do |h|
+  h.name = test_name
   puts "すでにデータは insert 済みなのでここには入らない"
 end
 
